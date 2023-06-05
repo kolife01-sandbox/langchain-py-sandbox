e1a13461d39840033aab8b7124f08585d70cb593
Kohei Tanaka <kohei69@gmail.com>
2023-06-05 20:14:28 +0900

    add quick start

---
+++ quickstart.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+from langchain.llms import OpenAI
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+llm = OpenAI(openai_api_key=openai_api_key)
+
+text = "What would be a good company name for a company that makes colorful socks?"
+print(llm(text))
