3ace30e598826b9c0ac7ca2436d305c346e9d082
Kohei Tanaka <kohei69@gmail.com>
2023-06-06 21:24:11 +0900

    fix prompt

--- src/chains.py
+++ src/chains.py
@@ -11,9 +11,9 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 
 llm = OpenAI(temperature=0.9)
 prompt = PromptTemplate(
-    input_variables=['product'],
-    template='What would be a good company name for a company that makes {product}?'
+    input_variables=['something'],
+    template='{something} に必要なスキルは何?'
 )
 
 chain = LLMChain(llm=llm, prompt=prompt)
-print(chain.run(product="colorful socks"))
+print(chain.run(something="AGI開発"))
