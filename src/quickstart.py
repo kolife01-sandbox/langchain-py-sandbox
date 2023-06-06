c9ee20d2bf6093f9df30f1fc19effec23b2f7775
Kohei Tanaka <kohei69@gmail.com>
2023-06-06 21:12:21 +0900

    mv file

--- src/quickstart.py
+++ src/quickstart.py
@@ -5,7 +5,7 @@ from langchain.llms import OpenAI
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
-llm = OpenAI(openai_api_key=openai_api_key)
+llm = OpenAI(openai_api_key=openai_api_key, temperature=0.9)
 
 text = "What would be a good company name for a company that makes colorful socks?"
 print(llm(text))
